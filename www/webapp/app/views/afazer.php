06647735c9713a8a831f6d56e9e40f2f7326c0dc
Sergio Moura <sergio.moura@sised.com.br>
2016-12-31 02:01:21 -0300

    Novos itens na lista de a fazeres.

--- www/webapp/app/views/afazer.php
+++ www/webapp/app/views/afazer.php
@@ -1,6 +1,30 @@
 
-	<ul>
-		<li>Tela de Cadastro de Usuários [2/1 a 4/1]</li>
+	<ul id="afazer">
+		<li>
+			Tela de login
+			<ul>
+				<li class="feito">Bloquear acesso a página interna sem login.</li>
+				<li>Fazer "Esqueci minha senha" [depois de definido o domínio para envio de email].</li>
+			</ul>
+		</li>
+		<li>
+			Tela Inicial:
+			<ul>
+				<li>Mostrar data de última atualização do sistema</li>
+				<li>Botão para Trocar senha/sair</li>
+			</ul>
+		</li>
+		<li>
+			Tela de Cadastro de Usuários [2/1 a 4/1]
+			<ul>
+				<li class="feito">Desenhar tela</li>
+				<li>Listar usuários do cliente</li>
+				<li>Adicionar um usuário</li>
+				<li>Inativar um usuário</li>
+				<li>Bloquear login de usuário inativo</li>
+				<li>Alterar um usuário preexistente</li>
+			</ul>
+		</li>
 		<li>Tela de Cadastro de Disciplinas [9/1 a 11/1] </li>
 		<li>Tela de Cadstro de Projetos [16/1 a 18/1]</li>
 		<li>Tela de Gestão de Documentos[21/1 a 25/1]</li>
