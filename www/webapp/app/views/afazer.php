3e44fc7abfad98feb392d0cdda9d298fd123c908
Sergio Moura <sergio.moura@sised.com.br>
2017-01-09 12:27:49 -0300

    Alteração no a fazeres

--- www/webapp/app/views/afazer.php
+++ www/webapp/app/views/afazer.php
@@ -16,26 +16,26 @@
 				<li>Fazer "Esqueci minha senha" [depois de definido o domínio para envio de email].</li>
 			</ul>
 		</li>
-		<li>
+		<li>Tela de Cadastro de Disciplinas [9/1 a 11/1] </li>
+		<li>Tela de Cadstro de Projetos [16/1 a 18/1]</li>
+		<li>Tela de Gestão de Documentos[21/1 a 25/1]</li>
+		<li>Exibir LOG de atividades dos usuários</li>
+		<li class="feiro">
 			Tela Inicial:
 			<ul>
 				<li class="feito">Mostrar data de última atualização do sistema</li>
 				<li class="feito">Botão para Trocar senha/sair</li>
 			</ul>
 		</li>
-		<li>
+		<li class="feito">
 			Tela de Cadastro de Usuários [2/1 a 4/1]
 			<ul>
 				<li class="feito">Desenhar tela</li>
-				<li>Listar usuários do cliente</li>
-				<li>Adicionar um usuário</li>
-				<li>Inativar um usuário</li>
-				<li>Bloquear login de usuário inativo</li>
-				<li>Alterar um usuário preexistente</li>
+				<li class="feito">Listar usuários do cliente</li>
+				<li class="feito">Adicionar um usuário</li>
+				<li class="feito">Inativar um usuário</li>
+				<li class="feito">Bloquear login de usuário inativo</li>
+				<li class="feito">Alterar um usuário preexistente</li>
 			</ul>
 		</li>
-		<li>Tela de Cadastro de Disciplinas [9/1 a 11/1] </li>
-		<li>Tela de Cadstro de Projetos [16/1 a 18/1]</li>
-		<li>Tela de Gestão de Documentos[21/1 a 25/1]</li>
-		<li>Exibir LOG de atividades dos usuários</li>
 	</ul>
