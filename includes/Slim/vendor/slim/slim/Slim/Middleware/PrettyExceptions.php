6c58db79b758f5c86fcbd2d52825629d68efa678
Sergio Moura <sergio.moura@sised.com.br>
2017-05-01 12:57:43 -0300

    Alterações no Slim para que as mensagens sejam mais legíveis em temas escuros dos navegadores.

--- includes/Slim/vendor/slim/slim/Slim/Middleware/PrettyExceptions.php
+++ includes/Slim/vendor/slim/slim/Slim/Middleware/PrettyExceptions.php
@@ -111,6 +111,6 @@ class PrettyExceptions extends \Slim\Middleware
             $html .= sprintf('<pre>%s</pre>', $trace);
         }
 
-        return sprintf("<html><head><title>%s</title><style>body{margin:0;padding:30px;font:12px/1.5 Helvetica,Arial,Verdana,sans-serif;}h1{margin:0;font-size:48px;font-weight:normal;line-height:48px;}strong{display:inline-block;width:65px;}</style></head><body>%s</body></html>", $title, $html);
+        return sprintf("<html><head><title>%s</title><style>body{color:#CCC;margin:0;padding:30px;font:12px/1.5 Helvetica,Arial,Verdana,sans-serif;}h1{margin:0;font-size:48px;font-weight:normal;line-height:48px;}strong{display:inline-block;width:65px;}</style></head><body>%s</body></html>", $title, $html);
     }
 }
