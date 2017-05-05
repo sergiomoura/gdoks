350d6c31425ca8d89df9fa5f379a4e780bae8d89
Sergio Moura <sergio.moura@sised.com.br>
2017-05-05 16:38:30 -0300

    Bug que impedia a execução de consultas sem parâmetros

--- includes/db.php
+++ includes/db.php
@@ -24,49 +24,52 @@
 			$n_args = func_num_args();
 			if($n_args == 2 || $n_args == 0){
 				throw new Exception("Number of arguments must be 1 or more then 2", 1);
+				return;
 			}
 
 			$stmt = $this->prepare($sql);
 			if($stmt === false){
 				throw new Exception("Failed to create statement: ".$this->error, 2);
+				return;
 			}
 
 			$n_params = $n_args - 2;
 			if($n_params > 0){
 				$types = func_get_arg(2);
 				call_user_func_array(array($stmt, 'bind_param'), $this->refValues(array_slice(func_get_args(),1)));
+			} else {
+				$stmt = $this->prepare($sql);
+			}
 
-				/* execute query */
-				if(!$stmt->execute()){throw new Exception("Failed to execute statement: ". $stmt->error, 3);}
-				
-				$metadata = $stmt->result_metadata();
-				
-				if($metadata === false) {
-					// consulta não retornou nenhum resultado.
-					$results = true;
-				} else {
-					// consulta retorna resultados;
-					$fields = $metadata->fetch_fields();
-					$args = array();
-					foreach($fields AS $field) {
-						$key = str_replace(' ', '_', $field->name); // space may be valid SQL, but not PHP
-						$args[$key] = &$field->name; // this way the array key is also preserved
-					}
-					call_user_func_array(array($stmt, "bind_result"), $args);
-					$results = array();
-					while($stmt->fetch()) {
-						$results[] = array_map(array($this,"copy_value"), $args);
-					}	
+			/* execute query */
+			if(!$stmt->execute()){throw new Exception("Failed to execute statement: ". $stmt->error, 3);}
+			
+			$metadata = $stmt->result_metadata();
+			
+			if($metadata === false) {
+				// consulta não retornou nenhum resultado.
+				$results = true;
+			} else {
+				// consulta retorna resultados;
+				$fields = $metadata->fetch_fields();
+				$args = array();
+				foreach($fields AS $field) {
+					$key = str_replace(' ', '_', $field->name); // space may be valid SQL, but not PHP
+					$args[$key] = &$field->name; // this way the array key is also preserved
 				}
-				
+				call_user_func_array(array($stmt, "bind_result"), $args);
+				$results = array();
+				while($stmt->fetch()) {
+					$results[] = array_map(array($this,"copy_value"), $args);
+				}	
+			}
+			
 
-				/* closing everithing */
-				$stmt->close();
+			/* closing everithing */
+			$stmt->close();
 
-				/* returning results */
-				return $results;
-
-			}		
+			/* returning results */
+			return $results;
 		}
 
 		private function refValues($arr){
