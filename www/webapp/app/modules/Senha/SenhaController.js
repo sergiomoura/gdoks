97a117ba0ff7c877756fc983c438f5bf2103415d
Sergio Moura <sergio.moura@sised.com.br>
2017-05-11 05:42:01 -0300

    Adequada a tela de alterar senha para MD

--- www/webapp/app/modules/Senha/SenhaController.js
+++ www/webapp/app/modules/Senha/SenhaController.js
@@ -1,35 +1,37 @@
 angular.module('Senha',[]).controller('SenhaController',SenhaController)
 
-function SenhaController($scope,$rootScope,GDoksFactory){
+function SenhaController($scope,$mdToast,GDoksFactory){
 	// Inicializando o objeto data;
 	$scope.data = {};
 	$scope.data.login = '';
 	$scope.data.senha1 = '';
 	$scope.data.senha2 = '';
-	$scope.obteve_resposta = false;
-	$scope.ok = false;
-	$scope.msg = '';
 
 	$scope.mudaLoginSenha = function(novoLogin,novaSenha){
 		GDoksFactory.mudaLoginSenha(novoLogin,novaSenha)
 			.success(
 				function(response){
-					$scope.obteve_resposta = true;
-					$scope.ok = true;
-					$scope.msg = 'Alterações realizadas com sucesso!';
+					$mdToast.show(
+						$mdToast.simple()
+						.textContent('Alterações realizadas com sucesso!')
+						.position('bottom left')
+						.hideDelay(5000)
+					);
 				}
 			)
 			.error(
 				function(error){
-					$scope.obteve_resposta = true;
-					$scope.ok = false;
-					$scope.msg = error.msg;
+					$mdToast.show(
+						$mdToast.simple()
+						.textContent(error.msg)
+						.position('bottom left')
+						.hideDelay(5000)
+					);
 				}
 			);
 	}
 
 	$scope.cancel = function(){
-		window.location = "WebGDoks.php#/home"
-		$scope.root.itemSelecionadoDoMenu = 0;
+		window.location = "WebGDoks.php#/home";
 	}
 }
