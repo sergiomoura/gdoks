ccdcc32c8823f17ebae75a0cbb52acfbd711abe3
Sergio Moura <sergio.moura@sised.com.br>
2017-06-27 01:11:09 -0300

    Adicionado teste de email

--- www/testes/mail.php
+++ www/testes/mail.php
@@ -1,3 +1,15 @@
 <?php 
-	mail("smouracalmon@gmail.com","teste","mensagem teste");
+	require 'vendor/autoload.php';
+	use Mailgun\Mailgun;
+
+	// First, instantiate the SDK with your API credentials
+	$mg = Mailgun::create('key-07bd64f71c14bf52a76c3e5e6d20d6da');
+
+	# Now, compose and send your message.
+	$mg->messages()->send('gdoks.com.br', [
+		'from'    => 'postmaster@gdoks.com.br', 
+		'to'      => 'smouracalmon@gmail.com', 
+		'subject' => 'The PHP SDK is awesome!', 
+		'text'    => 'It is so simple to send a message.'
+	]);
  ?>
