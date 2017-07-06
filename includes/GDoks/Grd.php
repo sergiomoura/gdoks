9365656d673446a435d8d75ef0ea0572475db6ae
Sergio Moura <sergio.moura@sised.com.br>
2017-07-06 10:49:10 -0300

    Parou de funcionar no servidor. Debug 6

--- includes/GDoks/Grd.php
+++ includes/GDoks/Grd.php
@@ -180,7 +180,7 @@
 					      WHERE id_grd=?) R
 					   INNER JOIN gdoks_pdas pdas ON pdas.id_revisao=R.id) c ON c.id_pda=a.id_pda';
 			$arquivos = array_map(function($a){return (object)$a;}, $this->db->query($sql,'i',$this->_id));
-
+			print_r($arquivos);
 			// Criando o arquivo zip na pasta raíz da empresa
 			$zip = new ZipArchive();
 			$caminhoZip = UPLOAD_PATH.$this->_id_empresa.'/'.$this->_codigo.'.zip';
@@ -200,6 +200,11 @@
 			
 			// Adicionando os arquivos da GRD
 			foreach ($arquivos as $f) {
+				if(file_exists(UPLOAD_PATH.$f->caminho)){
+					echo('Arquivo '.UPLOAD_PATH.$f->caminho.' existe<br>');
+				} else {
+					echo('Arquivo '.UPLOAD_PATH.$f->caminho.' NÃO existe<br>');
+				}
 				$zip->addFile(UPLOAD_PATH.$f->caminho,$pastaDeArquivos.'/'.$f->nome_cliente);
 			}
 
