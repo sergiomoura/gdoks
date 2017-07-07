934cbcdc1d9987c27010cf6243f0ecb09e4dd81c
Sergio Moura <sergio.moura@sised.com.br>
2017-07-07 01:36:08 -0300

    Tentando fazer funcionar no servidor - T15

--- includes/GDoks/Crypter.php
+++ includes/GDoks/Crypter.php
@@ -2,10 +2,10 @@
 	class Crypter {
 		
 		// Constantes internas
-		//private const CIPHER = 'aes-256-cbc';
-		//private const KEY = 'f51f87c023808d12413b037333543866d853aa55f76c779b7330eabab15b8b4c';
+		private static $CIPHER = 'aes-256-cbc';
+		private static $KEY = 'f51f87c023808d12413b037333543866d853aa55f76c779b7330eabab15b8b4c';
 
-		public static function crypt($data){/*
+		public static function crypt($data){
 			// Definindo vetor de inicialização
 			$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(Crypter::CIPHER));
 
@@ -16,18 +16,18 @@
 			$encrypted = $encrypted . ':' . base64_encode($iv);
 
 			// Retornando...
-			return $encrypted;*/
+			return $encrypted;
 		}
 
 		public static function decrypt($encrypted){
-			/*
+			
 			// recuperando a parte encriptada e o iv
 			$parts = explode(':',$encrypted);
 			if(sizeof($parts)!=2){
 				return false;
 			} else {
 				return @openssl_decrypt($parts[0], Crypter::CIPHER, hex2bin(Crypter::KEY), 0, base64_decode($parts[1]));
-			}*/
+			}
 		}
 	}
 ?>
