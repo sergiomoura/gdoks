de52d6e704bb67d6429ecbd578236cb67022b81c
Unknown <smouracalmon@gmail.com>
2017-07-26 05:47:43 -0300

    Script temporário para levantar acesso a BD

---
+++ www/testes/dbkeys.php
@@ -0,0 +1,6 @@
+<?php 
+	echo getenv('OPENSHIFT_MYSQL_DB_HOST'.'<br>');
+	echo getenv('OPENSHIFT_MYSQL_DB_PORT'.'<br>');
+	echo getenv('OPENSHIFT_MYSQL_DB_USERNAME'.'<br>');
+	echo getenv('OPENSHIFT_MYSQL_DB_PASSWORD'.'<br>');
+?>
