0a183cd637a13702b2b8db70a8e3b77e12944d7b
Sergio Moura <sergio.moura@sised.com.br>
2017-09-20 17:00:46 -0300

    Nova dbkey para a base de testes

--- client_data/gdoks/dbkey.php
+++ client_data/gdoks/dbkey.php
@@ -6,14 +6,14 @@
 		$dbkey->DB_HOST = "localhost";
 		$dbkey->DB_USER = "root";
 		$dbkey->DB_PASS = "vaiplaneta";
-		$dbkey->DB_BASE = "gdoks";
+		$dbkey->DB_BASE = "gdoks_000";
 		$dbkey->DB_PORT = 3306;
 		$dbkey->ID_EMPRESA = 1;
 	} else {
 		$dbkey->DB_HOST = getenv('OPENSHIFT_MYSQL_DB_HOST');
 		$dbkey->DB_USER = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 		$dbkey->DB_PASS = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-		$dbkey->DB_BASE = 'gdoks';
+		$dbkey->DB_BASE = 'gdoks_000';
 		$dbkey->DB_PORT = getenv('OPENSHIFT_MYSQL_DB_PORT');
 		$dbkey->ID_EMPRESA = 1;
 	}
