c2d314bafb06448bd880fbe62f555d594e609dc6
Sergio Moura <sergio.moura@sised.com.br>
2017-10-16 16:07:04 -0300

    Alterações nas informações para conexão com BD

--- client_data/gdoks/dbkey.php
+++ client_data/gdoks/dbkey.php
@@ -10,11 +10,11 @@
 		$dbkey->DB_PORT = 3306;
 		$dbkey->ID_EMPRESA = 1;
 	} else {
-		$dbkey->DB_HOST = getenv('OPENSHIFT_MYSQL_DB_HOST');
-		$dbkey->DB_USER = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-		$dbkey->DB_PASS = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-		$dbkey->DB_BASE = 'gdoks_000';
-		$dbkey->DB_PORT = getenv('OPENSHIFT_MYSQL_DB_PORT');
+		$dbkey->DB_HOST = "gdoks_001.mysql.dbaas.com.br";
+		$dbkey->DB_USER = "root";
+		$dbkey->DB_PASS = "k128#_gdoks";
+		$dbkey->DB_BASE = "gdoks_000";
+		$dbkey->DB_PORT = 3306;
 		$dbkey->ID_EMPRESA = 1;
 	}
 	
