cc73c9877f3213ea788b5cd86dfc198f1337ef72
Sergio Moura <sergio.moura@sised.com.br>
2017-10-16 16:19:48 -0300

    Desfazendo alterações do último commit. Preferi atualizar a versão do PHP no servidor

--- public_html/api/index.php
+++ public_html/api/index.php
@@ -26,7 +26,7 @@
 	// definindo função getallheaders caso ela não exista (caso NGINX)
 	if (!function_exists('getallheaders')){ 
 		function getallheaders(){ 
-			$headers = Array();
+			$headers = [];
 			foreach ($_SERVER as $name => $value){
 				if (substr($name, 0, 5) == 'HTTP_'){
 					$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
