979c05c10f89085421a83681058be1e864baf3ea
Sergio Moura <sergio.moura@sised.com.br>
2017-10-16 14:43:54 -0300

    Adequação para utilização do certificado compartilhado da locaweb

--- public_html/index.php
+++ public_html/index.php
@@ -2,6 +2,7 @@
 	if($_SERVER['SERVER_NAME'] == 'localhost'){
 		header("Location: /webapp/login.php");
 	} else {
-		header("Location: https://www.gdoks.com.br/webapp/login.php");
+		// header("Location: https://www.gdoks.com.br/webapp/login.php");
+		header("Location: https://gdoks.websiteseguro.com/webapp/login.php");
 	}
  ?>
