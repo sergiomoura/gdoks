bae8cddac5c74cd6e31cee7e3ed7a804c34aa1c1
Sergio Moura <sergio.moura@sised.com.br>
2017-10-21 03:27:30 -0300

    Adequação do redirecionamento inicial

--- www/index.php
+++ www/index.php
@@ -3,6 +3,6 @@
 		header("Location: /webapp/login.php");
 	} else {
 		// header("Location: https://www.gdoks.com.br/webapp/login.php");
-		header("Location: https://gdoks.websiteseguro.com/webapp/login.php");
+		header("Location: https://projeto-gdoks.appspot.com/webapp/login.php");
 	}
  ?>
