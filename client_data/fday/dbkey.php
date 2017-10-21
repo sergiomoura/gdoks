962d78de0387c263726f01a247761e5b3ea557c1
Sergio Moura <sergio.moura@sised.com.br>
2017-10-21 04:09:26 -0300

    Alterações no dbkey para acessar base de dados no GCLoud

--- client_data/fday/dbkey.php
+++ client_data/fday/dbkey.php
@@ -10,8 +10,8 @@
 		$dbkey->DB_PORT = 3306;
 		$dbkey->ID_EMPRESA = 1;
 	} else {
-		$dbkey->DB_HOST = "gdoks_001.mysql.dbaas.com.br";
-		$dbkey->DB_USER = "gdoks_001";
+		$dbkey->DB_HOST = "35.199.101.27";
+		$dbkey->DB_USER = "root";
 		$dbkey->DB_PASS = "k128#_gdoks";
 		$dbkey->DB_BASE = "gdoks_001";
 		$dbkey->DB_PORT = 3306;
--- client_data/gdoks/dbkey.php
+++ client_data/gdoks/dbkey.php
@@ -10,7 +10,7 @@
 		$dbkey->DB_PORT = 3306;
 		$dbkey->ID_EMPRESA = 1;
 	} else {
-		$dbkey->DB_HOST = "gdoks_001.mysql.dbaas.com.br";
+		$dbkey->DB_HOST = "35.199.101.27";
 		$dbkey->DB_USER = "root";
 		$dbkey->DB_PASS = "k128#_gdoks";
 		$dbkey->DB_BASE = "gdoks_000";
