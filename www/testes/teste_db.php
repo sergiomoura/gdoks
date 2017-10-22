61c1688dc47ba274b4cddc6ba4ea1a2bbbb0633d
Sergio Moura <sergio.moura@sised.com.br>
2017-10-22 16:03:55 -0300

    Tentando entrar em contato com a base de dados no Cloud Google

--- www/testes/teste_db.php
+++ www/testes/teste_db.php
@@ -1,10 +1,27 @@
 <?php
+
+	$dsn = getenv('MYSQL_DSN');
+	$user = getenv('MYSQL_USER');
+	$password = getenv('MYSQL_PASSWORD');
+
+	$db = new PDO($dsn,$user,$password);
+
+	$statement = $db->prepare("select * from gdoks_usuarios");
+	$statement->execute();
+	$all = $statement->fetchAll();
+
+	echo('<pre>');
+	print_r($all);
+	echo('</pre>');
+	die();
+	
+	/*
 	echo ('includindo ../../includes/db.php');
 	include('../../includes/db.php');
 
 	echo ('criando objeto chave ...');
 
-	/*
+	
 	$dbkey = new stdClass();
 	$dbkey->DB_HOST = "localhost";
 	$dbkey->DB_USER = "root";
@@ -12,7 +29,6 @@
 	$dbkey->DB_BASE = "gdoks_001";
 	$dbkey->DB_PORT = 3306;
 	$dbkey->ID_EMPRESA = 1;
-	*/
 	
 	$dbkey = new stdClass();
 	$dbkey->DB_HOST = "35.199.101.27";
@@ -29,4 +45,5 @@
 	print_r($rs);
 	echo('</pre>');
 	die();
+	*/
 ?>
