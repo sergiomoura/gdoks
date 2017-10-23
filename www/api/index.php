6e895e11d260bcc948efbf76bfa6f8e720eadf74
Sergio Moura <sergio.moura@sised.com.br>
2017-10-22 23:50:25 -0300

    Tentando fazer o slim funcionar no google cloud

--- www/api/index.php
+++ www/api/index.php
@@ -1,5 +1,7 @@
 <?php
 	
+	die('tentou executar a api!');
+
 	// Configurando a descrição do erro at runtime
 	error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 	ini_set('display_errors', 1);
