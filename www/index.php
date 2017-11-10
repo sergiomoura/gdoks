f41c636e762761b6f24bb57563c41005b07fde17
Sérgio Moura <smouracalmon@gmail.com>
2017-11-10 16:12:26 -0200

    Alterando o index para julgar o direcinamento com base no SERVER[HTTP_HOST], não mais no [SERVER_NAME]

--- www/index.php
+++ www/index.php
@@ -1,8 +1,7 @@
-<?php 
-	if($_SERVER['SERVER_NAME'] == 'localhost'){
+<?php
+	if(substr($_SERVER['HTTP_HOST'],0,9) == 'localhost'){
 		header("Location: /webapp/login.php");
 	} else {
-		// header("Location: https://www.gdoks.com.br/webapp/login.php");
-		header("Location: https://projeto-gdoks.appspot.com/webapp/login.php");
+		header("Location: https://www.gdoks.com.br/webapp/login.php");
 	}
  ?>
