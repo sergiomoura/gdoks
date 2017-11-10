6fe28947e5ac3892a015489b882ebca5e908baf1
Sérgio Moura <smouracalmon@gmail.com>
2017-11-10 16:28:14 -0200

    Registrando linha de comando que cria o container para o GDoks localmente

--- startGDoks.sh
+++ startGDoks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Comando que cria e roda o container
-# docker run --name gdoks-container -d -p 80:8080 -v $PWD/www:/usr/share/nginx/www -v $PWD/includes:/usr/share/nginx/includes -v $PWD/client_data:/usr/share/nginx/client_data gdoks-image:1.4
+# docker run --name gdoks-container -d -p 8080:8080 -v /var/run/mysqld:/localsql_socket -v $PWD/www:/usr/share/nginx/www -v $PWD/includes:/usr/share/nginx/includes -v $PWD/client_data:/usr/share/nginx/client_data gdoks-image:1.10
 
 #Comando que inicia o container já existente
 docker start gdoks-container
