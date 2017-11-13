5fd79073d261a620894e823e0ed0c7cf1965f7ce
Sérgio Moura <smouracalmon@gmail.com>
2017-11-13 14:46:05 -0200

    Consertando DBKeys para acesso a base de testes

--- client_data/gdoks/dbkey.php
+++ client_data/gdoks/dbkey.php
@@ -3,12 +3,12 @@
 	$dbkey = new stdClass();
 
 	if($_SERVER['SERVER_NAME'] == 'localhost'){
-		$dbkey->DB_HOST = 'localhost';
-		$dbkey->DB_USER = 'root';
-		$dbkey->DB_PASS = 'vaiplaneta';
-		$dbkey->DB_BASE = 'gdoks_000';
-		$dbkey->DB_PORT = 3306;
-		$dbkey->DB_SOCKET = null;
+		$dbkey->DB_HOST = null;
+		$dbkey->DB_USER = "root";
+		$dbkey->DB_PASS = "vaiplaneta";
+		$dbkey->DB_BASE = "gdoks_000";
+		$dbkey->DB_PORT = null;
+		$dbkey->DB_SOCKET = '/localsql_socket/mysqld.sock';
 		$dbkey->ID_EMPRESA = 1;
 	} else {
 		$dbkey->DB_HOST = null;
@@ -16,7 +16,7 @@
 		$dbkey->DB_PASS = 'k128#_gdoks';
 		$dbkey->DB_BASE = 'gdoks_000';
 		$dbkey->DB_PORT = null;
-		$dbkey->DB_SOCKET = '/cloudsql/projeto-gdoks:southamerica-east1:mysql-gdoks';
+		$dbkey->DB_SOCKET = '/cloudsql_socket/projeto-gdoks:southamerica-east1:mysql-gdoks';
 		$dbkey->ID_EMPRESA = 1;
 	}
 	
