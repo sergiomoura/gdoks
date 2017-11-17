c18f8b71cfc1affdda8524c6f6705bc5548afec4
Sérgio Moura <smouracalmon@gmail.com>
2017-11-17 06:00:46 -0200

    Alterações para funcionamento na máquina local

--- client_data/fday/dbkey.php
+++ client_data/fday/dbkey.php
@@ -2,14 +2,13 @@
 	include_once('GDoks/GDoks.php');
 	$dbkey = new stdClass();
 
-
 	if(GDoks::server() == GDoks::SERVER_LOCAL){
-		$dbkey->DB_HOST = null;
+		$dbkey->DB_HOST = 'localhost';
 		$dbkey->DB_USER = "root";
 		$dbkey->DB_PASS = "vaiplaneta";
 		$dbkey->DB_BASE = "gdoks_001";
 		$dbkey->DB_PORT = null;
-		$dbkey->DB_SOCKET = '/localsql_socket/mysqld.sock';
+		$dbkey->DB_SOCKET = null;
 		$dbkey->ID_EMPRESA = 1;
 	} else {
 		$dbkey->DB_HOST = 'gdoks001.mysql.dbaas.com.br';
--- client_data/gdoks/dbkey.php
+++ client_data/gdoks/dbkey.php
@@ -3,12 +3,12 @@
 	$dbkey = new stdClass();
 
 	if(substr($_SERVER['HTTP_HOST'],0,9) == 'localhost'){
-		$dbkey->DB_HOST = null;
-		$dbkey->DB_USER = "root";
-		$dbkey->DB_PASS = "vaiplaneta";
-		$dbkey->DB_BASE = "gdoks_000";
+		$dbkey->DB_HOST = 'localhost';
+		$dbkey->DB_USER = 'root';
+		$dbkey->DB_PASS = 'vaiplaneta';
+		$dbkey->DB_BASE = 'gdoks_000';
 		$dbkey->DB_PORT = null;
-		$dbkey->DB_SOCKET = '/localsql_socket/mysqld.sock';
+		$dbkey->DB_SOCKET = null;
 		$dbkey->ID_EMPRESA = 1;
 	} else {
 		$dbkey->DB_HOST = 'gdoks000.mysql.dbaas.com.br';
