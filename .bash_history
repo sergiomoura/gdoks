c79fcbd6c03554cf04680141454d0aee38beabd4
Sérgio Moura <smouracalmon@gmail.com>
2017-11-19 07:18:08 -0200

    Sincronizando pastas e dados com a locaweb para deploy automático

---
+++ .bash_history
@@ -0,0 +1,24 @@
+ls
+git
+clear
+git status
+nano
+apt-get install nano
+exit
+git --bare init
+ls
+ls -a
+rm -rf branches/ hooks/ objects/ description  info/ HEAD refs/
+ls
+mkdir .git
+cd .git/
+ls
+git --bare init
+cd ..
+ls
+ls -a
+ls
+ls -a
+rm -rf .git
+cd .ssh/
+ls
---
+++ public_html/testes/shellExec.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Shell Exec - Teste</title>
+</head>
+<body>
+	<pre>
+<?php 
+	$output = shell_exec("ls");
+	echo('Saída do shell_exec():'."\n" . $output."\n");
+	print_r($_SERVER);
+ ?>
+	 </pre>
+</body>
+</html>
