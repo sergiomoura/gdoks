031e7be5e5339b19a3a332cc31b60627e91dd900
Sérgio Moura <smouracalmon@gmail.com>
2017-11-27 07:48:54 -0200

    Consertando erro de digitação que impedia o pull.php de rodar (deploy automático)

--- www/webhooks/blocker.php
+++ www/webhooks/blocker.php
@@ -1,7 +1,7 @@
 <?php
 	// Definindo constante de token
-	include('contantes.php');
-
+	include('./constantes.php');
+	
 	// Matando o processo caso o token não seja válido
 	if(!array_key_exists('token', $_GET) || $_GET['token'] != TOKEN) die();
 ?>
