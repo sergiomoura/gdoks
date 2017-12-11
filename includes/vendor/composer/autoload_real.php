5f6bb8e3ac2deccedfd55321aec99bee4608490b
Sérgio Moura <smouracalmon@gmail.com>
2017-12-11 17:19:22 -0200

    Reinstalando o SendGrid com o seu php-http-client

--- includes/vendor/composer/autoload_real.php
+++ includes/vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit5c6c0aac8912ed69443575fd9320c1e0
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit5c6c0aac8912ed69443575fd9320c1e0::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire5c6c0aac8912ed69443575fd9320c1e0($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire5c6c0aac8912ed69443575fd9320c1e0($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
