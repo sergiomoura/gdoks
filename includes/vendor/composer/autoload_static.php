6197a39833eaf6bf81bbb60fb90a2cf713100f1e
Sérgio Moura <smouracalmon@gmail.com>
2017-12-11 17:16:04 -0200

    Removendo o pacote sendgrid - esse commit não envia emails.

--- includes/vendor/composer/autoload_static.php
+++ includes/vendor/composer/autoload_static.php
@@ -6,25 +6,6 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit5c6c0aac8912ed69443575fd9320c1e0
 {
-    public static $files = array (
-        '3f8bdd3b35094c73a26f0106e3c0f8b2' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/SendGrid.php',
-        '9dda55337a76a24e949fbcc5d905a2c7' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/helpers/mail/Mail.php',
-    );
-
-    public static $prefixLengthsPsr4 = array (
-        'S' => 
-        array (
-            'SendGrid\\' => 9,
-        ),
-    );
-
-    public static $prefixDirsPsr4 = array (
-        'SendGrid\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/sendgrid/php-http-client/lib',
-        ),
-    );
-
     public static $prefixesPsr0 = array (
         'S' => 
         array (
@@ -38,8 +19,6 @@ class ComposerStaticInit5c6c0aac8912ed69443575fd9320c1e0
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit5c6c0aac8912ed69443575fd9320c1e0::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit5c6c0aac8912ed69443575fd9320c1e0::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit5c6c0aac8912ed69443575fd9320c1e0::$prefixesPsr0;
 
         }, null, ClassLoader::class);
