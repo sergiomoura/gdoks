1b75fe745e3f60255aabe6d245b519365cfaf185
Sérgio Moura <smouracalmon@gmail.com>
2017-12-19 21:33:43 -0200

    Exibindo configurações do cliente na tela de configurações (print_r)

--- includes/GDoks/GDoks.php
+++ includes/GDoks/GDoks.php
@@ -10,5 +10,13 @@
 				return self::SERVER_GCLOUD;
 			}
 		}
+
+		public static function getConf($empresa){
+			// incluindo as constantes
+			include_once('constantes.php');
+
+			// carregando dados a serem retornados
+			return json_decode(file_get_contents(CLIENT_DATA_PATH.$empresa.'/config.json'));
+		}
 	}
  ?>
--- www/webapp/app/modules/Configuracoes/configuracoes.php
+++ www/webapp/app/modules/Configuracoes/configuracoes.php
@@ -1,1 +1,14 @@
-configuracoes.html
+<?php
+	// Incluindo o blocker de tela.
+	include('blocker_tela.php');
+	include('GDoks/GDoks.php');
+
+	// Carregando configurações da empresa
+	$conf = GDoks::getConf($user->empresa);
+
+	echo('<pre>');
+	print_r($conf);
+	echo('</pre>');
+	die();
+
+?>
