0835f6310c4f284c59f9b8b6607b774da1662523
Sérgio Moura <smouracalmon@gmail.com>
2018-01-07 09:33:36 -0200

    Na tela de últimas atualizações mostrando somente BUGFIX e NOVO

--- www/webapp/app/modules/VisaoGeral/visaogeral.php
+++ www/webapp/app/modules/VisaoGeral/visaogeral.php
@@ -2,6 +2,14 @@
 	<h3>Últimas Atualizações</h3>
 <pre>
 <?php
-	echo(str_ireplace('locaweb', 'X', str_replace(' -0200','',shell_exec('git log --date=iso8601 -n 100 --pretty=format:"[%h] %ci - %s" 2>&1')))); ?>
+	$output = str_ireplace('locaweb', 'X', str_replace(' -0200','',shell_exec('git log --date=iso8601 -n 100 --pretty=format:"[%h] %ci - %s" 2>&1')));
+	$ua = explode("\n", $output);
+	
+	foreach ($ua as $a) {
+		if(strpos($a, "NOVO:")>-1 || strpos($a, "BUGFIX:")>-1){
+			echo($a."\n");
+		}
+	}
+?>
 </pre>
 </div>
