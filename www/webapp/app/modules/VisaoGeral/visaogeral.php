c456858334562a7b4c7ee7ff729daa528bf63ea0
Sérgio Moura <smouracalmon@gmail.com>
2018-02-05 14:15:43 -0200

    BUGFIX: Escondendo o '%' enquanto informações dos gráficos não carregam

--- www/webapp/app/modules/VisaoGeral/visaogeral.php
+++ www/webapp/app/modules/VisaoGeral/visaogeral.php
@@ -1,7 +1,7 @@
 <div class="visaogeral_container">
 	<div class="progressoGeral">
 		<canvas id="progressoGeral_canvas" width="100%" height="100%" ></canvas>
-		<div class="numero">{{progresso_geral}}%</div>
+		<div class="numero" ng-if="progresso_geral!=null">{{progresso_geral}}%</div>
 	</div>
 
 	<div class="documentos">
