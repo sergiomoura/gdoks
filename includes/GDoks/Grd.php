7d62492803e0daf5a3dcf7371e7e5bc5415cf710
Sérgio Moura <smouracalmon@gmail.com>
2018-03-02 09:06:51 -0300

    TRY 4 - troquei o readfile pelo readfile_chunked

--- includes/GDoks/Grd.php
+++ includes/GDoks/Grd.php
@@ -368,6 +368,32 @@
 			return $caminhoZip;
 		}
 
+		// Função que lê arquivos aos pedaços
+		private function readfile_chunked($filename,$retbytes=true) { 
+			$chunksize = 1*(1024*1024); // how many bytes per chunk 
+			$buffer = ''; 
+			$cnt =0; 
+			
+			$handle = fopen($filename, 'rb');
+			if ($handle === false) {
+				return false;
+			}
+			while (!feof($handle)) {
+				$buffer = fread($handle, $chunksize);
+				echo $buffer;
+				ob_flush();
+				flush();
+				if ($retbytes) {
+					$cnt += strlen($buffer);
+				}
+			}
+			$status = fclose($handle); 
+			if ($retbytes && $status) { 
+				return $cnt; // return num. bytes delivered like readfile() does. 
+			} 
+			return $status; 
+		}
+
 		// Função que envia o zip para o cliente
 		public function sendZip($nome_do_emissor,$sem_compressao=false){
 			// Gerando o zip
@@ -390,7 +416,7 @@
 
 			// output the file first clean mem
 			ob_end_clean();
-			readfile($caminhoDoZip);
+			$this->readfile_chunked($caminhoDoZip);
 			
 			// apagando o zip
 			unlink($caminhoDoZip);
