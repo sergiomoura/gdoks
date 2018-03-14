c94040e2748b0004bf2b5d69549acb8804bb02ef
Sérgio Moura <smouracalmon@gmail.com>
2018-03-14 09:43:17 -0300

    Criada área do cliente da Genese Engenharia

---
+++ www/clientes/genese/index.php
@@ -0,0 +1,8 @@
+<?php
+	// Definições de constantes
+	define('CODIGO_EMPRESA','gdoks');
+	define('NOME_EMPRESA','GDoks');
+
+	// requerindo o sistema da área do cliente
+	require('../system.php');
+?>
