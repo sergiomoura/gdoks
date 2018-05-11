90cf665d61904eb7dd91c327d45fd9b9fd5c0db5
Sérgio Moura <smouracalmon@gmail.com>
2018-05-11 05:29:26 -0300

    Desenhada tela de proposta

--- www/webapp/app/modules/Propostas/proposta.php
+++ www/webapp/app/modules/Propostas/proposta.php
@@ -1,3 +1,106 @@
-<pre>
-{{proposta|json}}
-</pre>
+<?php
+ 
+    // Incluindo classe "Tela" para carregar as opções da tela Documentos
+    require 'GDoks/Tela.php';
+
+    // Levantando dados do usuário a partir do cookie
+    $user = json_decode($_COOKIE['user']);
+
+    // Incluindo constantes
+    require_once 'constantes.php';
+    require_once 'db.php';
+
+    // Carregando dbkey
+    require_once CLIENT_DATA_PATH.$user->empresa.'/dbkey.php';
+
+    // Criando conexão com base de dados
+    $db = new DB($dbkey);
+    unset($dbkey);
+
+    // Criando o objeto Tela
+    try {
+    	$tela = Tela::CreateById(13, $user->id, $db);
+    } catch (Exception $e) {
+    	echo($e->getMessage());
+    	exit(1);
+    }
+
+?>
+<div class="bloco_central_80" id="proposta_container">
+	{{proposta|json}}	
+	<div layout="row" layout-align="space-between center" class="controles">
+		<div class="search">
+			<md-input-container>
+				<label>Código da Proposta</label>
+				<input type="text" ng-model="proposta.codigo" ng-disabled="proposta.id!=0" focus>
+			</md-input-container>
+			<md-input-container class="selectContainer">
+				<label>Cliente</label>
+				<md-select ng-model="proposta.cliente" ng-disabled="proposta.id!=0">
+				  <md-option ng-value="c" ng-repeat="c in clientes">{{ c.nome }}</md-option>
+				</md-select>
+			</md-input-container>
+			<md-button
+					class="md-raised md-primary"
+					ng-if="proposta.id==0"
+					ngf-multiple="false"
+					ngf-select
+					ngf-max-size="<?php echo(ini_get('upload_max_filesize').'B'); ?>"
+					ngf-model-invalid="errorFiles"
+					ng-model="proposta.arquivo"
+					aria-label="Selecione arquivos">Selecione Arquivo (Máximo <?php echo(ini_get('upload_max_filesize').'B)'); ?></md-button>
+			<md-button ng-if="proposta.id==0" class="md-raised md-primary" aria-label="Salvar proposta"><md-icon class="material-icons step">save</md-icon>Salvar</md-button>
+			<md-button ng-if="proposta.id!=0 && proposta.versoes.length==0" class="md-raised md-accent" aria-label="Remover proposta"><md-icon class="material-icons step">delete</md-icon>Apagar proposta</md-button>
+		</div>
+		<md-button ng-disabled="proposta.id==0" class="md-raised md-primary" aria-label="Nova versão"><md-icon class="material-icons step">add</md-icon>Nova Versão</md-button>
+	</div>
+
+    <table ng-if="proposta.versoes.length > 0">
+    	<thead>
+    		<tr>
+    			<td>Versão</td>
+    			<td>Criação</td>
+    			<td>Emissão</td>
+    			<td>Aprovação</td>
+    			<td></td>
+    		</tr>
+    	</thead>
+    	<tbody>
+    		<tr ng-repeat="v in proposta.versoes">
+    			<td>{{v.serial}}</td>
+    			<td>{{v.criacao | date:'dd/MM/yyyy'}}</td>
+    			<td>{{v.emissao | date:'dd/MM/yyyy'}}</td>
+    			<td>{{v.aprovacao | date:'dd/MM/yyyy'}}</td>
+    			<td>
+    				<md-button ng-if="$last && (v.emissao==null)" class="md-icon-button" ng-disabled="false" aria-label="Emitir versão para cliente">
+    					<md-icon class="material-icons step">delete</md-icon>
+    					<md-tooltip md-delay="400" md-direction="bottom" md-autohide="true">
+    						Remover versão de proposta
+    					</md-tooltip>
+    				</md-button>
+
+    				<md-button class="md-icon-button" ng-disabled="false" aria-label="Emitir versão para cliente">
+    					<md-icon class="material-icons step">send</md-icon>
+    					<md-tooltip md-delay="400" md-direction="bottom" md-autohide="true">
+    						Emitir versão para cliente
+    					</md-tooltip>
+    				</md-button>
+    				
+    				<md-button class="md-icon-button" ng-disabled="false" aria-label="Download de arquivo">
+    					<md-icon class="material-icons step">file_download</md-icon>
+    					<md-tooltip md-delay="400" md-direction="bottom" md-autohide="true">
+    						Baixar arquivo
+    					</md-tooltip>
+    				</md-button>
+
+    				<md-button class="md-icon-button" ng-disabled="false" aria-label="Aprovar">
+    					<md-icon class="material-icons step">thumb_up_alt</md-icon>
+    					<md-tooltip md-delay="400" md-direction="bottom" md-autohide="true">
+    						Aprovar versão de proposta
+    					</md-tooltip>
+    				</md-button>
+    			</td>
+    		</tr>
+    	</tbody>
+    </table>
+</div>
--- www/webapp/dist/scripts.js
+++ www/webapp/dist/scripts.js
@@ -26501,24 +26501,81 @@ http://trix-editor.org/
 	}
 
 	function PropostaController($scope,GDoksFactory,$routeParams,$mdToast){
+
+		// Lendo id da proposta do routParam
 		var id_proposta = $routeParams.id;
 
-		GDoksFactory.getProposta(id_proposta)
+		// Definindo valores iniciais para proposta e clientes
+		$scope.proposta = null;
+		$scope.clientes = null;
+
+		// Carregando clientes da base de dados
+		GDoksFactory.getClientes()
 		.success(function(response){
-			$scope.proposta = response.proposta;
+			$scope.clientes = response.clientes;
+			parseProposta();
 		})
 		.error(function(error){
 			// Retornando Toast para o usuário
 			$mdToast.show(
 				$mdToast.simple()
-				.textContent('Não foi possível carregar a proposta: '+error.msg)
-				.position('bottom left')
+				.textContent(error.msg)
+				.position('bottom left',error.msg)
 				.hideDelay(5000)
 			);
 
 			// Imprimindo erro no console
 			console.warn(error);
-		})
+		});
+
+		// Verificando se é uma nova proposta ou se é uma proposta existente
+		if(id_proposta == 0){
+			$scope.proposta = {
+				id:0,
+				id_cliente:0,
+				codigo:'',
+				versoes:[]
+			}
+		} else {
+			// Carregando proposta da base de dados
+			GDoksFactory.getProposta(id_proposta)
+			.success(function(response){
+				$scope.proposta = response.proposta;
+				parseProposta();
+			})
+			.error(function(error){
+				// Retornando Toast para o usuário
+				$mdToast.show(
+					$mdToast.simple()
+					.textContent('Não foi possível carregar a proposta: '+error.msg)
+					.position('bottom left')
+					.hideDelay(5000)
+				);
+
+				// Imprimindo erro no console
+				console.warn(error);
+			});
+		}
+
+		function parseProposta(){
+			if($scope.proposta != null && $scope.clientes!= null){
+				// Atribuindo cliente
+				if($scope.proposta.id_cliente != 0){
+					$scope.proposta.cliente = $scope.clientes.find(function(c){return c.id==this},$scope.proposta.id_cliente);
+				} else {
+					$scope.proposta.cliente = null;
+				}
+
+				// Parsing datas de versões
+				var v;
+				for (var i = $scope.proposta.versoes.length - 1; i >= 0; i--) {
+					v = $scope.proposta.versoes[i];
+					v.criacao = new Date(v.criacao);
+					v.emissao = new Date(v.emissao);
+					v.aprovacao = (v.aprovacao == null? null : new Date(v.aprovacao));
+				}
+			}
+		}
 	}
 
 })();
